14e22be81ea15937de61e6bbe8421e0c7c74130c
Charlotte Millet <charlottemillet04@gmail.com>
2023-03-12 17:59:17 +0100

    add creation of environment

--- Launcher.bat
+++ Launcher.bat
@@ -1,5 +1,6 @@
 set root=C:\Users\charl\anaconda3
 call %root%\Scripts\activate.bat %root%
+call conda env create -f environment.yml
 call conda activate env_elec
 call jupyter notebook notebook_elections.ipynb
 pause
